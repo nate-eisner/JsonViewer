4abb61a16251f06a95e024d5ee0f805461ed1369
Nate Eisner <nate@eisner.io>
2021-03-16 09:08:08 -0700

    Update project to androidx and latest gradle plugin

--- jsonviewer/src/main/java/com/yuyh/jsonviewer/library/view/JsonItemView.java
+++ jsonviewer/src/main/java/com/yuyh/jsonviewer/library/view/JsonItemView.java
@@ -1,7 +1,7 @@
 package com.yuyh.jsonviewer.library.view;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
